4aea17deca3e687c20bdccd5cdaec65e87e90e35
Jayendra <jayendra06mishra@gmail.com>
2026-04-18 13:22:20 +0530

    update Thank you message

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ def home():
     return """
     <h1>Hello from DevOps 🚀</h1>
     <p>Welcome to DevOps world</p>
-    """
+    <p>Thank you !!!</p>
+
+ """
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
